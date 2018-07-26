9a5496b3292fb4516672c69c483f23ff0b7d7c67
JohnZBurnett <tuninator@gmail.com>
2018-07-26 17:19:06 -0400

    build migrates and seeds

--- db/migrate/20180726210829_create_articles.rb
+++ db/migrate/20180726210829_create_articles.rb
@@ -1,7 +1,11 @@
 class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
-
+      t.string :title
+      t.string :body_text
+      t.string :thumbnail_url
+      t.string :picture_url
+      t.string :publish_time
       t.timestamps
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+first_user = User.create(username: "firstusername", password: "password")
+first_article = Article.create(title: "Test Title", body_test: "test body", thumbnail_url: "test", picture_url: "test", publish_time: "20XX")
+first_join = UserArticle.create(user_id: first_user.id, article_id: first_article.id)
+
